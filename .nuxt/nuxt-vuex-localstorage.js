4457861ecebb4556f148accd5549f9795532ca65
m1raclee4u <64864409+m1raclee4u@users.noreply.github.com>
2023-03-19 22:20:24 +0300

    почистил репо

--- .nuxt/nuxt-vuex-localstorage.js
+++
@@ -1,6 +0,0 @@
-import bindStorage from 'nuxt-vuex-localstorage/plugins/bindStorage'
-
-export default (ctx) => {
-  const options = {"mode":"debug","localStorage":["posts"]}
-  bindStorage(ctx, options)
-}
